0d6165dda92c5ff54418757c923f341c0db34a76
Paula Gearon <pgearon@cisco.com>
2017-12-06 12:29:24 -0500

    Simplified slightly

--- src/aoc/day6.clj
+++ src/aoc/day6.clj
@@ -2,34 +2,33 @@
   (:require [clojure.java.io :refer [resource]]
             [clojure.string :as str]))
 
+(defn offset-max
+  [s]
+  (reduce (fn [[maxn maxv] [n v]] (if (> v maxv) [n v] [maxn maxv]))
+          [0 0]
+          (map-indexed vector s)))
+
 (defn redistribute
-  [offset array]
-  (let [size (count array)
-        v (array offset) ]
+  [array]
+  (let [[offset v] (offset-max array)
+        size (count array)]
     (loop [i (inc offset)
            arr (update array offset (constantly 0))]
       (if (> i (+ offset v))
         arr
         (recur (inc i) (update arr (mod i size) inc))))))
 
-(defn offset-max
-  [s]
-  (first
-    (reduce (fn [[maxn maxv] [n v]] (if (> v maxv) [n v] [maxn maxv]))
-            [0 0]
-            (map-indexed vector s))))
-
 (defn day6 [data]
   (loop [c 0 seen #{} banks (vec data)]
     (if (seen banks)
       c
-      (recur (inc c) (conj seen banks) (redistribute (offset-max banks) banks)))) )
+      (recur (inc c) (conj seen banks) (redistribute banks)))) )
 
 (defn day6* [data]
   (loop [c 0 seen {} banks (vec data)]
     (if-let [last-count (seen banks)]
       (- c last-count)
-      (recur (inc c) (assoc seen banks c) (redistribute (offset-max banks) banks)))))
+      (recur (inc c) (assoc seen banks c) (redistribute banks)))))
 
 (defn -main [& args]
   (let [line (-> (resource "day6.dat")

a727d70941acf4a6d5460af01edbc377481f7bf8
Paula Gearon <pgearon@cisco.com>
2017-12-23 15:22:43 -0500

    Second star

--- src/aoc/day23.clj
+++ src/aoc/day23.clj
@@ -6,28 +6,28 @@
 (defn val-for [state x] (if (keyword? x) (state x 0) x))
 
 (defn setn
-  [state s2 x y]
-  [(assoc state x y) s2])
+  [state x y]
+  (assoc state x y))
 
-(defn -n [a b] (if a (- a b) b))
+(defn -n [a b] (if a (- a b) (- b)))
 
 (defn sub
-  [state s2 x y]
-  [(update state x -n y) s2])
+  [state x y]
+  (update state x -n y))
 
 (defn *n [a b] (if a (* a b) 0))
 
 (defn mul
-  [state s2 x y]
-  [(-> state
-       (update x *n y)
-       (update :muls inc)) s2])
+  [state x y]
+  (-> state
+      (update x *n y)
+      (update :muls inc)))
 
 (defn jnz
-  [{ip :ip :as state} s2 x y]
-  [(if (not= (val-for state x) 0)
-      (update state :ip + y -1)
-      state) s2])
+  [{ip :ip :as state} x y]
+  (if (not= (val-for state x) 0)
+    (update state :ip + y -1)
+    state))
 
 (def fns
   {"set" setn
@@ -53,40 +53,53 @@
     [opfn a1k a2k]))
 
 (defn calc-step
-  ([instrs ip this-state]
-   (first (calc-step instrs ip this-state nil)))
-  ([instrs ip this-state other-state]
-   (let [[op a1 a2] (instrs ip)
-         a2' (if (keyword? a2) (this-state a2 0) a2)
-         [nxt-state next-ostate] (op this-state other-state a1 a2')]
-     [(update nxt-state :ip inc) next-ostate])))
+  [instrs {ip :ip :as this-state}]
+  (let [[op a1 a2] (instrs ip)
+        a2' (if (keyword? a2) (this-state a2 0) a2)
+        nxt-state (op this-state a1 a2')]
+    (update nxt-state :ip inc)))
 
 (def init-state {:ip 0 :muls 0})
 
 (defn oor? [ip len] (or (neg? ip) (>= ip len)))
 
 (defn run
-  ([program] (run program nil))
-  ([program init]
-   (let [instrs (into [] (map parse program))
-         program-length (count instrs)]
-     (loop [{ip :ip :as state} (merge init-state init) c 0]
-       (if (oor? ip program-length)
-         
-         (:muls state)
-         (recur (calc-step instrs ip state) (inc c)))))))
+  [program]
+  (let [instrs (into [] (map parse program))
+        program-length (count instrs)]
+    (loop [{ip :ip :as state} init-state]
+      (if (oor? ip program-length)
+        state
+        (recur (calc-step instrs state))))))
+
+;; run this from a repl to determine ongoing state
+(defn run*
+  [program init]
+  (let [instrs (into [] (map parse program))
+        program-length (count instrs)]
+    (loop [{ip :ip :as state} (merge init-state init) c 0]
+      (println state ":-" (instrs ip))
+      (if (or (and (>= c 30) (= ip 11)) (oor? ip program-length))
+        state
+        (recur (calc-step instrs state) (inc c))))))
 
 (defn day23
   [program]
-  (run program))
+  (:muls (run program)))
 
 (defn day23*
-  [program]
-  (run program {:a 1}))
+  "Steps b from 16500 to 123500 (inclusive) by 17. For each b, loop e and d from 2 to b,
+   and check if they divide into b. If they do, then set f to 0, and increment h.
+   Prime numbers of b do not have divisors, so h will not be incremented."
+  []
+  (let [ptext (slurp (resource "primes.txt"))
+        t (str/split ptext #"\s+")
+        prime? (clojure.core/set (map #(Long/parseLong %) t))]
+   (count (remove prime? (range 106500 123517 17)))))
 
 (defn -main [& args]
   (let [lines (-> (resource "day23.dat")
                   slurp
                   str/trim
                   (str/split #"\n"))]
-    (println (day23 lines) ";" )))
+    (println (day23 lines) ";" (day23*))))
